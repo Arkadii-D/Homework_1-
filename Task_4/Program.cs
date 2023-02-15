487ba8433ab1e5323243d93be0be3adc242cebdd
Arkadii Demin <miniadres@rambler.ru>
2023-02-15 22:46:41 +0300

    Task 4 commit

---
+++ Task_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int numC = int.Parse(Console.ReadLine());
+
+if (numA > numB)
+{
+    if (numA >  numC)
+    {
+        Console.WriteLine("Максимальное число: " + numA);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + numC);
+    }
+}
+
+else if (numB > numC)
+{
+    Console.WriteLine("Максимальное число: " + numB);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + numC);
+}
